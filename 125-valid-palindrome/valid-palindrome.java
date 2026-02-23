ac1d81943320289ef32383c8611a7c4e896cf25a
skrishnanz <s.krishnan2409@gmail.com>
2026-02-23 21:19:26 +0530

    Time: 13 ms (36.63%) | Memory: 46.3 MB (32.32%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,7 +3,6 @@ class Solution {
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
         int left = 0;
         int right = s.length() - 1;
-
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
