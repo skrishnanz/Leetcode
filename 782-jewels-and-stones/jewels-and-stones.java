240cf7cb6717c4164e34065b98ee0bb01dac790c
skrishnanz <s.krishnan2409@gmail.com>
2026-03-09 20:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (50.59%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        int n=jewels.length();
+        int m=stones.length();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(jewels.charAt(i)==stones.charAt(j)){
+                    count++;
+                }
+            
+            }
+            
+        }
+        return count;
+    }
+}
