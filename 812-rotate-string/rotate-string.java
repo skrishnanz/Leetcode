401676cf3c61502be3fdcbf45c48d803c0ac8c2f
skrishnanz <krishnancode@gmail.com>
2026-08-31 09:52:07 +0530

    Time: 2 ms (64.95%) | Memory: 43.3 MB (82.82%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean rotateString(String s ,String g) 
+    {
+        if(s.length()!=g.length())return false;
+            
+        return (s+s).contains(g);
+             
+    }
+    
+}
