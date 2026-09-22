008926a3ab3b3263bc1f728a0547628fab72d791
skrishnanz <krishnancode@gmail.com>
2026-09-22 09:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (22.24%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2 * n];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            result[index] = nums[i];     
+            result[index + 1] = nums[i + n]; 
+            index += 2;
+        }
+        return result;
+    }
+}
